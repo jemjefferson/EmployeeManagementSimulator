21e163ec143e7a1c1ac7b032cc5da3be46db78cb
Jordan Jefferson <jejefferson@students.ntc.edu>
2023-01-15 10:18:57 -0600

    Changed logo and resized image.

--- EmployeeManagementSimulator/obj/Debug/EmployeeManagementSimulator_Content.g.i.cs
+++ EmployeeManagementSimulator/obj/Debug/EmployeeManagementSimulator_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ntc_college.jpeg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("em.png")]
 
 
